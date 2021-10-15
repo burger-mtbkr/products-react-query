65e05b5b0692fc70a961d0d280095e3b37624d01
Lóan Burger <loan.burger@gmail.com>
2021-10-16 10:33:15 +1300

    Style product edit form and add validation

--- project/src/models/product.model.ts
+++ project/src/models/product.model.ts
@@ -1,3 +1,6 @@
+import * as yup from 'yup';
+import { SchemaOf } from 'yup';
+
 export type Product = {
   id: number;
   name: string;
@@ -9,3 +12,17 @@ export interface IProductState {
   deleteModalOpen: boolean;
   selectedProducts: Product[];
 }
+
+export const productSchema: SchemaOf<Product> = yup
+  .object()
+  .shape({
+    id: yup.number().positive().required(),
+    name: yup.string().required('Name is required'),
+    category: yup.string().required('Category is required'),
+    price: yup
+      .number()
+      .typeError('Price is required')
+      .min(0, 'Price must be greater than 0')
+      .required('Price is required'),
+  })
+  .required();
--- project/src/views/Products/ProductForm.tsx
+++ project/src/views/Products/ProductForm.tsx
@@ -1,16 +1,24 @@
-import TextField from '@mui/material/TextField';
+import { TextField, Button, Grid } from '@mui/material';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useSelector } from 'react-redux';
-import { Product } from 'src/models';
+import { Product, productSchema } from 'src/models';
 import { getEditProduct } from 'src/selectors';
+import { useHistory } from 'react-router-dom';
 
 const ProductForm = (): JSX.Element => {
+  const history = useHistory();
+  const product = useSelector(getEditProduct);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Product>();
-  const product = useSelector(getEditProduct);
+  } = useForm<Product>({
+    mode: 'all',
+    reValidateMode: 'onChange',
+    resolver: yupResolver(productSchema),
+  });
 
   const onSubmit: SubmitHandler<Product> = (data: Product) => {
     console.log(data);
@@ -18,39 +26,73 @@ const ProductForm = (): JSX.Element => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <TextField
-        type="hidden"
-        label="Name"
-        variant="outlined"
-        value={product.id}
-        {...register('id', { required: true })}
-      />
-      <TextField
-        type="text"
-        label="Name"
-        variant="outlined"
-        defaultValue={product.name}
-        {...register('name', { required: true, maxLength: 20 })}
-      />
-      <p>{errors.name?.type === 'required' && 'Name is required'}</p>
-      <TextField
-        type="text"
-        label="Category"
-        variant="outlined"
-        defaultValue={product.category}
-        {...register('category', { required: true, maxLength: 20 })}
-      />
-      <p>{errors.category?.type === 'required' && 'Category is required'}</p>
-      <TextField
-        type="number"
-        label="Price"
-        variant="outlined"
-        defaultValue={product.price}
-        {...register('price', { required: true })}
-      />
-      <p>{errors.price?.type === 'required' && 'Price is required'}</p>
-      <input type="submit" />
-      <input type="reset" />
+      <Grid container direction="column" justifyContent="center" spacing={1}>
+        <input type="hidden" value={product.id} {...register('id')} />
+        <Grid item xs={6} marginY={2}>
+          <TextField
+            type="text"
+            label="Name"
+            variant="outlined"
+            defaultValue={product.name}
+            InputLabelProps={{ shrink: true }}
+            error={errors.name !== undefined}
+            helperText={errors.name?.message}
+            {...register('name', { required: true, maxLength: 20 })}
+          />
+        </Grid>
+        <Grid item xs={6} marginY={2}>
+          <TextField
+            type="text"
+            label="Category"
+            variant="outlined"
+            defaultValue={product.category}
+            InputLabelProps={{ shrink: true }}
+            error={errors.category !== undefined}
+            helperText={errors.category?.message}
+            {...register('category', { required: true, maxLength: 20 })}
+          />
+        </Grid>
+        <Grid item xs={6} marginY={2}>
+          <TextField
+            type="number"
+            label="Price"
+            variant="outlined"
+            defaultValue={product.price}
+            InputLabelProps={{ shrink: true }}
+            inputProps={{
+              inputMode: 'numeric',
+              pattern: '[0-9]*',
+            }}
+            error={errors.price !== undefined}
+            helperText={errors.price?.message}
+            {...register('price', { required: true })}
+          />
+        </Grid>
+        <Grid
+          container
+          xs={12}
+          spacing={2}
+          marginY={2}
+          direction="row"
+          justifyContent="center"
+        >
+          <Grid item>
+            <Button variant="contained" type="submit">
+              Submit
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" type="reset">
+              Reset
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" onClick={() => history.replace('/')}>
+              Cancel
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
     </form>
   );
 };
