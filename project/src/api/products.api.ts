d267bd0a32ae373d99d868085f4c98227b7c423a
Lóan Burger <loan.burger@gmail.com>
2021-10-17 09:50:35 +1300

    Add Api code for get, add, edit, delete

--- project/src/api/products.api.ts
+++ project/src/api/products.api.ts
@@ -15,3 +15,67 @@ export const getAllProducts = async (): Promise<Product[]> => {
   }
   return [];
 };
+
+export const getProduct = async (id: string): Promise<Product | undefined> => {
+  const url = process.env.REACT_APP_API_END_POINT;
+  if (url) {
+    const response = await axios.get(`${url}/${id}`, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return response.data as Product;
+  }
+  return undefined;
+};
+
+export const postProduct = async (
+  product: Product,
+): Promise<Product | undefined> => {
+  const url = process.env.REACT_APP_API_END_POINT;
+  if (url) {
+    const response = await axios.post(`${url}`, product, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data as Product;
+  }
+  return undefined;
+};
+
+export const putProduct = async (
+  product: Product,
+): Promise<Product | undefined> => {
+  const url = process.env.REACT_APP_API_END_POINT;
+  if (url) {
+    const response = await axios.put(`${url}/${product.id}`, product, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data as Product;
+  }
+  return undefined;
+};
+
+export const deleteProduct = async (
+  id: string,
+): Promise<Product | undefined> => {
+  const url = process.env.REACT_APP_API_END_POINT;
+  if (url) {
+    const response = await axios.delete(`${url}/${id}`, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return response.data as Product;
+  }
+  return undefined;
+};
