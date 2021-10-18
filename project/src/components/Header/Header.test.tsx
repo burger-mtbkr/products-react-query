b3f63d4e6b7045c2bf0bf4763e64fcd60779a777
Loan Burger <7775769+loanburger@users.noreply.github.com>
2021-10-18 20:35:16 +1300

    Rtl wrapper (#6)
    * Adding RTL Wrapper
    
    * fuck babel
    
    * Use renderWithRedux wrapper

--- project/src/components/Header/Header.test.tsx
+++ project/src/components/Header/Header.test.tsx
@@ -1,29 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from 'src/redux/store';
+import { screen } from '@testing-library/react';
+import { rootInitialState } from 'src/reducers';
+import { renderWithRedux } from 'src/test/utils';
 import { TestIds } from 'src/utils/testId.util';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import Header from './Header';
 
 test('renders welcome test', () => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: false,
-      },
-      mutations: {
-        retry: false,
-      },
-    },
+  renderWithRedux(<Header />, {
+    initialState: rootInitialState,
   });
-
-  render(
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Header />
-      </QueryClientProvider>
-    </Provider>,
-  );
   const linkElement = screen.getByTestId(TestIds.headerAppBarTestId);
   expect(linkElement).toBeInTheDocument();
 });
--- project/src/views/Home/Home.test.tsx
+++ project/src/views/Home/Home.test.tsx
@@ -1,42 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
+import { screen } from '@testing-library/react';
 import ProductTable from 'src/components/Products/ProductTable/ProductTable';
-import { store } from 'src/redux/store';
 import { TestIds } from 'src/utils';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { renderWithRedux } from 'src/test/utils';
+import { rootInitialState } from 'src/reducers';
 import Home from './Home';
 
 describe(`${Home.name} tests`, () => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: false,
-      },
-      mutations: {
-        retry: false,
-      },
-    },
-  });
-
   test(`renders the ${Home.name} view`, () => {
-    render(
-      <Provider store={store}>
-        <QueryClientProvider client={queryClient}>
-          <Home />
-        </QueryClientProvider>
-      </Provider>,
-    );
+    renderWithRedux(<Home />, {
+      initialState: rootInitialState,
+    });
     const linkElement = screen.getByTestId(TestIds.homeViewComponent);
     expect(linkElement).toBeInTheDocument();
   });
   test(`renders the ${ProductTable.name} view`, () => {
-    render(
-      <Provider store={store}>
-        <QueryClientProvider client={queryClient}>
-          <ProductTable />
-        </QueryClientProvider>
-      </Provider>,
-    );
+    renderWithRedux(<ProductTable />, {
+      initialState: rootInitialState,
+    });
+
     const linkElement = screen.getByTestId(TestIds.productTable);
     expect(linkElement).toBeInTheDocument();
   });
